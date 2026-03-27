fd541d717508e4249bdcc97736fb6c48958f41d5
黄建霖 <3335433588@qq.com>
2026-03-27 10:01:46 +0800

    Initial Commit

---
+++ BTCTickerTests/BTCTickerTests.swift
@@ -0,0 +1,19 @@
+//
+//  BTCTickerTests.swift
+//  BTCTickerTests
+//
+//  Created by hjl on 2026/3/27.
+//
+
+import Testing
+@testable import BTCTicker
+
+struct BTCTickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
